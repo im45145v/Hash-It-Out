c271dfcb54fd61e1416602ca53cc64b3f3c603b7
Prem Kiran Laknaboina <premlaknaboina@gmail.com>
2023-10-06 05:02:51 +0000

    Basic Template

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for, session
-import pyrebase
-from pymongo import MongoClient
-app = Flask(__name__)
-app.secret_key = os.environ.get('SECRET_KEY','1324456789')
-client = MongoClient(os.getenv('mongostr'), serverSelectionTimeoutMS=60000)
-db = client[""]
 
+
+app = Flask(__name__)
+
+# Home page
+@app.route('/')
+def index():
+    return render_template('dashboard.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
